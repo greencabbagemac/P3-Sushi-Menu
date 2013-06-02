6ffccb8d151f42d90f71679f4e9fd2cfffcb06e1
Sina Mandaquit <kaueokeanuinui@hotmail.com>
2013-06-02 14:52:36 -0700

    Menu Bar Operational
    *Added hide and show for menu
    *Pictures move on command

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,29 @@ $(document).ready(function() {
 	//$('.thumbnail > p').hover(function(){
 		//$('.price').fadeToggle();
 		//});
+	$('#slick-hide-raw').click(function() {
+		$('.veg').hide('slow');
+		$('.spicy').hide('slow');
+		$('.reg').hide('slow');
+		$('.raw').show('fast');
+		return false;
+	});
 	
+	$('#slick-hide-veg').click(function() {
+		$('.spicy').hide('slow');
+		$('.reg').hide('slow');
+		$('.raw').hide('slow');
+		$('.veg').show('fast');
+
+		return false;
+	});
 	
-	
+	$('#slick-hide-spicy').click(function() {
+		$('.veg').hide('slow');
+		$('.reg').hide('slow');
+		$('.raw').hide('slow');
+		$('.spicy').show('fast');		
+		return false;
+	});
 	
 });
